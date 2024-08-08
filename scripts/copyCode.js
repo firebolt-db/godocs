9f83cb3269d2a0b2410ade06b737817a35bcae3d
arushi-firebolt <146018150+arushi-firebolt@users.noreply.github.com>
2024-08-07 17:02:48 -0700

    Adding clipboard ability to examples

---
+++ scripts/copyCode.js
@@ -0,0 +1,23 @@
+var codeBlocks = document.querySelectorAll('pre.highlight');
+
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerHTML = '<img src="../../assets/images/copy_icon.svg" />';
+
+  codeBlock.append(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerHTML = '<img src="../../assets/images/success.svg" />';
+    var twoSeconds = 2000;
+
+    setTimeout(function () {
+      copyButton.innerHTML = '<img src="../../assets/images/copy_icon.svg" />';
+    }, twoSeconds);
+  });
+});
