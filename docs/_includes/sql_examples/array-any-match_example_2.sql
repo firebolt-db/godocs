c7cc32dc5672fa70375c85c5614e0c61db5480b2
nanimal-fb <papia.nandi@firebolt.io>
2025-02-26 15:56:19 -0800

    Removed merge conflict marker

--- docs/_includes/sql_examples/array-any-match_example_2.sql
+++ docs/_includes/sql_examples/array-any-match_example_2.sql
@@ -2,8 +2,4 @@ SELECT ARRAY_ANY_MATCH([])          as empty,
     ARRAY_ANY_MATCH([true])         as single_true,
     ARRAY_ANY_MATCH([false])        as single_false,
     ARRAY_ANY_MATCH([NULL])         as single_null ,
-<<<<<<< HEAD
     ARRAY_ANY_MATCH([false, NULL])  as false_and_null;
-=======
-    ARRAY_ANY_MATCH([false, NULL])  as false_and_null;
->>>>>>> gh-pages
